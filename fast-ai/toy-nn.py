5aadca1ad08db524e0cc77596774dd818f044e30
dupe <njdupe@gmail.com>
2023-05-03 20:58:30 -0400

    notes for toy nn

---
+++ fast-ai/toy-nn.py
@@ -0,0 +1,13 @@
+"""
+    Giga simple "neural network" with one layer, two ReLU neurons.
+
+    Mirrors the fast.ai lecture 3 Titanic excel spreadsheet example.
+
+    Plans:
+        1. Read data from titanic csv using pandas
+        2. Transform data into ML friendly formats (e.g. encode categorical data, normalize continuous data)
+        3. Define mse loss function
+        4. Define two sets of weights and biases
+        5. Define two ReLU neurons (one for each set of weights and biases)
+        6. Train the network on the training data
+"""
